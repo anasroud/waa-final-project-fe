3ef4184ee519d512cabf5cb83e285dc667705275
Anas Roud <roud2@hotmail.com>
2025-03-20 02:22:54 -0500

    remove script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,9 @@
-/* eslint-disable @next/next/no-sync-scripts */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          crossOrigin="anonymous"
-          src="//unpkg.com/react-scan/dist/auto.global.js"
-        />
-      </Head>
+      <Head></Head>
       <body className="antialiased">
         <Main />
         <NextScript />
