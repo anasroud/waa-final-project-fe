e6d0abf96eb1168f3e84dd17ebc1d1fae857a72e
JoeEverest <everestjoseph11@yahoo.com>
2025-03-20 14:04:56 -0500

    refactor: remove unnecessary ScrollArea component from customer dashboard

--- src/pages/dashboard/customer/index.tsx
+++ src/pages/dashboard/customer/index.tsx
@@ -2,7 +2,6 @@ import CustomerOffersTable from "@/components/CustomerOffersTable/CustomerOffers
 import FavoritedPropertiesTable from "@/components/FavoritedPropertiesTable/FavoritedPropertiesTable";
 import Nav from "@/components/Nav/Nav";
 import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { HeartIcon, CircleDollarSign } from "lucide-react";
 
@@ -15,33 +14,30 @@ export default function CustomerDashboard() {
           <h1 className="text-3xl font-bold">Buyer Dashboard</h1>
         </div>
         <Tabs defaultValue="tab-1">
-          <ScrollArea>
-            <TabsList className="text-foreground mb-3 h-auto gap-2 rounded-none border-b bg-transparent px-0 py-1">
-              <TabsTrigger
-                value="tab-1"
-                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent  after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-              >
-                <CircleDollarSign
-                  className="-ms-0.5 me-1.5 opacity-60"
-                  size={16}
-                  aria-hidden="true"
-                />
-                My Offersrelative
-              </TabsTrigger>
-              <TabsTrigger
-                value="tab-2"
-                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-              >
-                <HeartIcon
-                  className="-ms-0.5 me-1.5 opacity-60"
-                  size={16}
-                  aria-hidden="true"
-                />
-                Favorites
-              </TabsTrigger>
-            </TabsList>
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
+          <TabsList className="text-foreground mb-3 h-auto gap-2 rounded-none border-b bg-transparent px-0 py-1">
+            <TabsTrigger
+              value="tab-1"
+              className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent  after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              <CircleDollarSign
+                className="-ms-0.5 me-1.5 opacity-60"
+                size={16}
+                aria-hidden="true"
+              />
+              My Offers
+            </TabsTrigger>
+            <TabsTrigger
+              value="tab-2"
+              className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              <HeartIcon
+                className="-ms-0.5 me-1.5 opacity-60"
+                size={16}
+                aria-hidden="true"
+              />
+              Favorites
+            </TabsTrigger>
+          </TabsList>
           <TabsContent value="tab-1">
             <CustomerOffersTable />
           </TabsContent>
