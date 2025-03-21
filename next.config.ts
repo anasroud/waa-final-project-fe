60f61e81b20195bd1267f465be0f383cbab4de79
Anas Roud <roud2@hotmail.com>
2025-03-21 12:48:21 -0500

    fix image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  images: {
+    domains: ["d188cc59d2yje5.cloudfront.net"],
+  },
 };
 
 export default nextConfig;
